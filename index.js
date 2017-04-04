39897a6ca30fab18f83eab1a05125b1c8bf9594a
skiilaa <moriczgergo05@gmail.com>
2017-04-04 20:19:53 +0200

    Ported TucoChat to Gitter!

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
-var tmi = require('tmi.js');
+// Module requirements
+var Gitter = require("node-gitter");
 var net = require('net');
-const spawn = require('child_process').spawn;
-const channelName = "skiilaa";
-var tClient = new net.Socket();
 
+// Channel to join
+const channelName = "TucoFlyer/dev";
+
+// Parse config file
 var options = require("./config.json");
 
-var client = new tmi.client(options);
+// Initialize TCP client
+var tClient = new net.Socket();
 
-const debug = false; //Change to true to enable verbose mode
+// Initialize Gitter API
+var gitter = new Gitter(options.token);
 
-var novote = false; //Change to true to disable voting on script start
+const debug = false; // Change to true to enable verbose mode
 
-var voted = [];
+var novote = false; // Change to true to disable voting on script start
 
+var voted = []; // This is where the users who voted are stored
+
+// Vote letters
+var dirlett = ["W", "A" , "S", "D"];
+var rotlett = ["U", "H", "J", "K"];
+
+// Vote scores
 var dirvotes = [0, 0, 0, 0];
 var rotvotes = [0, 0, 0, 0];
 
+// Store empty vote objects
 var emptyDirVotes = dirvotes;
 var emptyRotVotes = rotvotes;
 
-var dirlett = ["W", "A" , "S", "D"];
-var rotlett = ["U", "H", "J", "K"];
+// Connect to TCP server
+tClient.connect(1337, '127.0.0.1', function(){
+	console.log("Connected to TCP server.");
+});
 
 setInterval(function(){
     if (!novote){
@@ -90,87 +104,82 @@ setInterval(function(){
     }
 }, 2*1000);
 
-tClient.connect(1337, '127.0.0.1', function(){
-    console.log("Connected");
-});
+gitter.rooms.join(channelName).then(function(room) {
+	console.log("Connected to " + channelName + "!");
+	var events = room.streaming().chatMessages();
 
-client.connect();
-
-client.on('connected', function(address, port){
-    console.log('Logged in! ' + address + ":" + port);
-});
-
-client.on('message', function(channel, userstate, message, self) {
-    if (message.toLowerCase().startsWith('!move')) {
-        if (novote){
-            client.say(channelName, "Voting is currently disabled.");
-        } else {
-            if (message == "!move" || message == "!move " || message.length < "!move W U".length){
-                client.say(channelName, "Usage:\n`!move <direction> <rotation>`\n`<direction>`: `W`, `A`, `S` or `D`\n`<rotation>`: `U`, `H`, `J` or `K`");
-            } else {
-                var dir = message.charAt(6).toUpperCase();
-                var rot = message.charAt(8).toUpperCase();
-                var valid = true;
-                var nopr = false;
-                var vdir = true;
-                var vrot = true;
-                if (dir != "W" && dir != "A" && dir != "S" && dir != "D" && dir != "X"){
-                    if (debug){
-                        console.log(userstate["display-name"] + ": Direction format invalid.");
-                    }
-                    valid = false;
-                }
-                if (rot != "U" && rot != "H" && rot != "J" && rot != "K" && rot != "X"){
-                    if (debug){
-                        console.log(userstate["display-name"] + ": Rotation format invalid.");
-                    }
-                    valid = false;
-                }
-                if (dir == "X"){
-                    vdir = false;
-                }
-                if (rot == "X"){
-                    vrot = false;
-                }
-                voted.forEach(function(item){
-                    if (userstate["display-name"]){
-                        if (debug){
-                            console.log(userstate["display-name"] + " already voted.");
-                        }
-                        valid = false;
-                        nopr = true;
-                    }
-                });
-                if (valid){
-                    voted.push(userstate["display-name"]);
-                    console.log(userstate["display-name"] + " voted " + dir + rot);
-                    if (vdir){
-                        dirlett.forEach(function(item, index){
-                            if (dir == item){
-                                dirvotes[index]++;
-                            }
-                        });
-                    }
-                    if (vrot){
-                        rotlett.forEach(function(item,index){
-                            if (rot == item){
-                                rotvotes[index]++;
-                            }
-                        });
-                    }
-                } else {
-                    if (!nopr){
-                        client.say(channelName, "Usage:\n`!move <direction> <rotation>`\n`<direction>`: `W`, `A`, `S` or `D`\n`<rotation>`: `U`, `H`, `J` or `K`");
-                    }
-                }
-            }
-        }
-    } else if (message == "!togglevote"){
-        if (userstate["display-name"] == "skiilaa"){
-            novote = !novote;
-            client.say(channelName, "Voting has been set to " + !novote + ".");
-        } else {
-            client.say(channelName, "You have no permission to do that.");
-        }
-    }
+	events.on('chatMessages', function(message){
+		if (message.model.text.toLowerCase().startsWith('!move')) {
+	        if (novote){
+    	        room.send("Voting is currently disabled.");
+        	} else {
+            	if (message.model.text == "!move" || message.model.text == "!move " || message.model.text.length < "!move W U".length){
+                	room.send("Usage:\n`!move <direction> <rotation>`\n`<direction>`: `W`, `A`, `S` or `D`\n`<rotation>`: `U`, `H`, `J` or `K`");
+	            } else {
+    	            var dir = message.model.text.charAt(6).toUpperCase();
+        	        var rot = message.model.text.charAt(8).toUpperCase();
+            	    var valid = true;
+                	var nopr = false;
+	                var vdir = true;
+    	            var vrot = true;
+        	        if (dir != "W" && dir != "A" && dir != "S" && dir != "D" && dir != "X"){
+             	       if (debug){
+                	        console.log(message.model.fromUser.id + ": Direction format invalid.");
+                    	}
+	                    valid = false;
+    	            }
+        	        if (rot != "U" && rot != "H" && rot != "J" && rot != "K" && rot != "X"){
+            	        if (debug){
+                	        console.log(message.model.fromUser.id + ": Rotation format invalid.");
+                    	}
+	                    valid = false;
+    	            }
+        	        if (dir == "X"){
+            	        vdir = false;
+                	}
+	                if (rot == "X"){
+    	                vrot = false;
+        	        }
+            	    voted.forEach(function(item){
+                	    if (message.model.fromUser.id){
+                    	    if (debug){
+                        	    console.log(message.model.fromUser.id + " already voted.");
+	                        }
+    	                    valid = false;
+        	                nopr = true;
+            	        }
+                	});
+	                if (valid){
+    	                voted.push(message.model.fromUser.id);
+        	            console.log(message.model.fromUser.id + " voted " + dir + rot);
+            	        if (vdir){
+                	        dirlett.forEach(function(item, index){
+                    	        if (dir == item){
+                        	        dirvotes[index]++;
+                            	}
+	                        });
+    	                }
+        	            if (vrot){
+            	            rotlett.forEach(function(item,index){
+                	            if (rot == item){
+                    	            rotvotes[index]++;
+                        	    }
+                        	});
+	                    }
+    	            } else {
+        	            if (!nopr){
+            	            room.send("Usage:\n`!move <direction> <rotation>`\n`<direction>`: `W`, `A`, `S` or `D`\n`<rotation>`: `U`, `H`, `J` or `K`");
+                	    }
+                	}
+	            }
+    	    }
+	    } else if (message.model.text == "!togglevote"){
+    	    if (message.model.fromUser.id == options.adminId){
+        	    novote = !novote;
+            	room.send("Voting has been set to " + !novote + ".");
+	        } else {
+    	        room.send("You have no permission to do that.");
+        	}
+    	}
+	});
 });
